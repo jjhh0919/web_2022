3da99f1239d6f171da48dc234fa7a80074ded751
Mun JunHyeok <jjhh0919@gmail.com>
2022-10-10 11:54:51 +0900

    DataStructure(Singly Linked Lists(insert, remove, reverse, print))

--- JavaScript/DataStructure/singlyLinkedLists.js
+++ JavaScript/DataStructure/singlyLinkedLists.js
@@ -23,6 +23,7 @@ class SinglyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
   push(val) {
     const newNode = new Node(val);
     if (!this.head) {
@@ -35,6 +36,7 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
   pop() {
     if (!this.head) return undefined;
     let current = this.head;
@@ -63,15 +65,15 @@ class SinglyLinkedList {
     }
     return currentHead;
   }
+
   unshift(val) {
     const newNode = new Node(val);
     if (!this.head) {
       this.head = newNode;
       this.tail = this.head;
-    } else {
-      newNode.next = this.head;
-      this.head = newNode;
     }
+    newNode.next = this.head;
+    this.head = newNode;
     this.length++;
     return this;
   }
@@ -86,6 +88,7 @@ class SinglyLinkedList {
     }
     return current;
   }
+
   set(index, val) {
     let foundNode = this.get(index);
     if (foundNode) {
@@ -93,6 +96,55 @@ class SinglyLinkedList {
       return true;
     }
     return false;
+  }
+
+  insert(index, val) {
+    if (index < 0 || index > this.length) return false;
+    if (index === this.length) return !!this.push(val);
+    if (index === 0) return !!this.unshift(val);
+
+    const newNode = new Node(val);
+    const prev = this.get(index - 1);
+    const temp = prev.next;
+    prev.next = newNode;
+    newNode.next = temp;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    if (index === 0) return this.shift();
+    if (index === this.length - 1) return this.pop();
+    const previousNode = this.get(index - 1);
+    const removed = previousNode.next;
+    previousNode.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
+  print() {
+    const arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
   }
 }
 
@@ -104,5 +156,15 @@ list.pop();
 list.shift();
 list.unshift("fourth");
 list.unshift("fifth");
+list.insert(3, "sixth");
+list.remove(2);
+list.reverse();
+list.print();
 
-console.log(list);
+/*
+시간복잡도
+삽입 : O(1)
+삭제 : 위치에 따라 O(1) or O(n)
+검색 : O(n)
+접근 : O(n)
+*/
