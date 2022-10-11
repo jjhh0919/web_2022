64403249873da2f1660a74b4c56cdb872133b48c
Mun JunHyeok <jjhh0919@gmail.com>
2022-10-11 11:18:37 +0900

    DataStructure(Doubly Linked Lists(push, pop))

---
+++ JavaScript/DataStructure/doublyLinkedLists.js
@@ -0,0 +1,54 @@
+/*
+단일 연결 리스트와 유사하지만 리스트에 있는 모든 노드들은 앞으로 가는 포인터도 있지만 뒤로 가는 포인터도 있음
+*/
+
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    const newNode = new Node(val);
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      newNode.prev = this.tail;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+  pop() {
+    if (!this.head) return undefined;
+    const poppedNode = this.tail;
+    if (this.length === 1) {
+      this.head = null;
+      this.tail = null;
+    } else {
+      this.tail = poppedNode.prev;
+      this.tail.next = null;
+      poppedNode.prev = null;
+    }
+    this.length--;
+    return poppedNode;
+  }
+}
+
+const list = new DoublyLinkedList();
+list.push("first");
+list.push("second");
+list.push("third");
+list.pop();
+
+console.log(list);
