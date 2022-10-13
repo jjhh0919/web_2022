d277c2f4a8adb921ba323cef56e0aff3d0565d73
Mun JunHyeok <jjhh0919@gmail.com>
2022-10-13 14:57:23 +0900

    DataStructure(Doubly Linked Lists(get, set))

--- JavaScript/DataStructure/doublyLinkedLists.js
+++ JavaScript/DataStructure/doublyLinkedLists.js
@@ -64,6 +64,34 @@ class DoublyLinkedList {
     this.length++;
     return this;
   }
+  get(index) {
+    if (index < 0 || index >= this.length) return null;
+    let count, current;
+    if (index <= this.length / 2) {
+      count = 0;
+      current = this.head;
+      while (count !== index) {
+        current = current.next;
+        count++;
+      }
+    } else {
+      count = this.length - 1;
+      current = this.tail;
+      while (count !== index) {
+        current = current.prev;
+        count--;
+      }
+    }
+    return current;
+  }
+  set(index, val) {
+    const foundNode = this.get(index);
+    if (foundNode != null) {
+      foundNode.val = val;
+      return true;
+    }
+    return false;
+  }
 }
 
 const list = new DoublyLinkedList();
